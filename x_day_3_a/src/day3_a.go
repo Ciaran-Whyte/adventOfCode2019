92e24d5674269143787d173b5adef59e3a200267
Ciaran Whyte <ciaran.whyte@fireeye.com>
2019-12-03 17:01:54 +0000

    day3_a go port

--- x_day_3_a/src/day3_a.go
+++ x_day_3_a/src/day3_a.go
@@ -3,28 +3,58 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
 
-type lines struct {
-	lineA []string
-	lineB []string
+type point struct {
+	x int
+	y int
 }
 
-func readLineInputsFromFile(fileName string) (linesToReturn lines) {
+type line struct {
+	start point
+	end   point
+}
+
+func parseCords(inputStrings []string) []line {
+	cords := []point{}
+	cords = append(cords, point{x: 0, y: 0})
+	idx := 0
+
+	fmt.Println(inputStrings)
+	for _, x := range inputStrings {
+		m, _ := strconv.Atoi(x[1:])
+		if x[0] == 'R' {
+			cords = append(cords, point{x: cords[idx].x + m, y: cords[idx].y})
+		} else if x[0] == 'L' {
+			cords = append(cords, point{x: cords[idx].x - m, y: cords[idx].y})
+		} else if x[0] == 'U' {
+			cords = append(cords, point{x: cords[idx].x, y: cords[idx].y + m})
+		} else if x[0] == 'D' {
+			cords = append(cords, point{x: cords[idx].x, y: cords[idx].y - m})
+		}
+	}
+
+	expandedLines := []line{}
+	for idx, x := range cords {
+		if idx+1 < len(cords) {
+			expandedLines = append(expandedLines, line{start: x, end: cords[idx+1]})
+		}
+	}
+	return expandedLines
+}
+
+func readLineInputsFromFile(fileName string) ([]line, []line) {
 	input, _ := ioutil.ReadFile(fileName)
-	twoLines := strings.Split(string(input), "\n") 
-	linesToReturn.lineA = strings.Split(string(twoLines[0]), ",")
-	linesToReturn.lineB = strings.Split(string(twoLines[1]), ",")
-	return linesToReturn
-}
-
-func parseLinesToPoints(line []string) (points []string){
-	
+	twoLines := strings.Split(string(input), "\n")
+	lineA := parseCords(strings.Split(string(twoLines[0]), ","))
+	lineB := parseCords(strings.Split(string(twoLines[1]), ","))
+	return lineA, lineB
 }
 
 func main() {
-	inputLines := readLineInputsFromFile("/Users/ciaran.whyte/dev/adventofcode2019/x_day_3_a/src/input.txt")
-	fmt.Println(inputLines.lineA)
-	fmt.Println(inputLines.lineB)
+	lineA, lineB := readLineInputsFromFile("/Users/ciaran.whyte/dev/adventofcode2019/x_day_3_a/src/input.txt")
+	fmt.Println(lineA)
+	fmt.Println(lineB)
 }
